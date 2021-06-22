b9baa20c2ca0b098f3bc06ffbf89962460b39776
Anant7393 <anantmishra7393@gmail.com>
2021-06-23 02:13:12 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+
+
+function initMap() {
+  // The location of Uluru
+  const uluru = { lat: 28.621310, lng: 77.3654 };
+  // The map, centered at Uluru
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4,
+    center: uluru,
+  });
+  // The marker, positioned at Uluru
+  const marker = new google.maps.Marker({
+    position: uluru,
+    map: map,
+  });
+}
